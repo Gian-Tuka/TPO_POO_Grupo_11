4632ad7d617118a4c88da9bf907d40c426952fec
NicolasdeZucca <zuccarellonj@gmail.com>
2026-06-13 13:57:00 -0300

    Añado controlador de productos y servicios

--- src/main/java/Farmared/controller/item/ControladorProductosYServicios.java
+++ src/main/java/Farmared/controller/item/ControladorProductosYServicios.java
@@ -1,4 +1,16 @@
 package Farmared.controller.item;
 
+import Farmared.model.item.Item;
+import Farmared.model.item.Producto;
+import Farmared.model.item.Servicio;
+import Farmared.model.item.UnidadDeMedida;
+import Farmared.model.precio.PrecioProveedor;
+
+import java.util.ArrayList;
+
 public class ControladorProductosYServicios {
+    private ArrayList<Producto> productos;
+    private ArrayList<Servicio> servicios;
+    private ArrayList<UnidadDeMedida> unidadesDeMedida;
+    private ArrayList<PrecioProveedor> precioPorItem;
 }
