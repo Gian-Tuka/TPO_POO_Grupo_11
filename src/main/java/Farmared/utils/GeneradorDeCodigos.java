a33aa2000650fb3d7b8a3bc8e8987ed33a41fbf5
GianTuka <glavinia@uade.edu.ar>
2026-06-20 14:27:24 -0300

    Actualizacion en la generacion de codigos

--- src/main/java/Farmared/utils/GeneradorDeCodigos.java
+++ src/main/java/Farmared/utils/GeneradorDeCodigos.java
@@ -1,20 +1,37 @@
 package Farmared.utils;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class GeneradorDeCodigos {
 
+    private static final Set<String> codigosExistentes = new HashSet<String>();
+
     public String generarCodigo(String codigoAlfabetico, int cantidadDeNumeros) {
+        String nuevoCodigo = "";
+
+        do {
+            nuevoCodigo = generarCodigoInterno(codigoAlfabetico, cantidadDeNumeros);
+        } while (codigosExistentes.contains(nuevoCodigo));
+
+        codigosExistentes.add(nuevoCodigo);
+        return nuevoCodigo;
+    }
+
+    private String generarCodigoInterno(String parteAlfabetica, int cantidadNumerica) {
         String codigoNumerico = "";
         Random random = new Random();
 
-        for (int i = 0; i < cantidadDeNumeros; i++) {
+        for (int i = 0; i < cantidadNumerica; i++) {
             int numero = random.nextInt(10);
             codigoNumerico = codigoNumerico + numero;
         }
 
-        return codigoAlfabetico + "-" + codigoNumerico;
+        return parteAlfabetica + "-" + codigoNumerico;
     }
+
+
 }
 
 
